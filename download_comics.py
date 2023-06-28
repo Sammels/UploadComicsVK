ac67105439bfb2d27a578a44134b2a0fa7ab3d07
Sammels <Sasagete@gmail.com>
2023-06-28 06:53:22 +0300

    add const variable and refactor code

--- download_comics.py
+++ download_comics.py
@@ -1,14 +1,16 @@
 import os
 import requests
 from pathlib import Path
-
 from dotenv import load_dotenv
 from python_utils import download_comics
 
 
+VK_API_GROUP_GET_METHOD = "https://api.vk.com/method/groups.get"
+VK_API_PHOTO_WALL_UPLOAD_SERVER = "https://api.vk.com/method/photos.getWallUploadServer"
+VK_API_LOAD_PHOTO = "https://api.vk.com/method/photos.saveWallPhoto"
+
 def get_groups_vk(vk_client_id: str, vk_access_token: str) -> str:
     """Function POST request to VK API used GROUP.GET method """
-    vk_api_group_get_method = "https://api.vk.com/method/groups.get"
 
     headers = {
         "Authorization": f"Bearer {vk_access_token}"
@@ -17,7 +19,7 @@ def get_groups_vk(vk_client_id: str, vk_access_token: str) -> str:
         "user_ids": vk_client_id,
         "v": 5.131
     }
-    response = requests.post(vk_api_group_get_method, headers=headers, params=params)
+    response = requests.post(VK_API_GROUP_GET_METHOD, headers=headers, params=params)
     response.raise_for_status()
 
     text = response.json()
@@ -26,7 +28,6 @@ def get_groups_vk(vk_client_id: str, vk_access_token: str) -> str:
 
 def get_wall_vk_upload_server(vk_client_id: str, vk_access_token: str) -> str:
     """Function POST request to VK API used PHOTO.WALL.UPLOAD.SERVER method """
-    vk_api_photo_wall_upload_server = "https://api.vk.com/method/photos.getWallUploadServer"
     headers = {
         "Authorization": f"Bearer {vk_access_token}"
     }
@@ -34,14 +35,14 @@ def get_wall_vk_upload_server(vk_client_id: str, vk_access_token: str) -> str:
         "group_id": vk_client_id,
         "v": 5.131
     }
-    response = requests.post(vk_api_photo_wall_upload_server, headers=headers, params=params)
+    response = requests.post(VK_API_PHOTO_WALL_UPLOAD_SERVER, headers=headers, params=params)
     response.raise_for_status()
 
     text = response.json()
     return text
 
 
-def upload_photo_to_vk(url_link: str, filename: str)-> str:
+def upload_photo_to_vk(url_link: str, filename: str) -> str:
     """Function load photo to vk server and return dict photo info"""
     with open(f"Files/{filename}", 'rb') as file:
         url = url_link
@@ -56,7 +57,6 @@ def upload_photo_to_vk(url_link: str, filename: str)-> str:
 
 
 def photo_save_wall_vk(group_id, photo_info, vk_access_token):
-    vk_api_load_photo = "https://api.vk.com/method/photos.saveWallPhoto"
 
     header = {
         "Authorization": f"Bearer {vk_access_token}"
@@ -69,14 +69,15 @@ def photo_save_wall_vk(group_id, photo_info, vk_access_token):
         "v": 5.131
     }
 
-    response = requests.post(vk_api_load_photo, params=params, headers=header)
+    response = requests.post(VK_API_LOAD_PHOTO, params=params, headers=header)
     response.raise_for_status()
 
     save_photo_wall_info = response.json()
     return save_photo_wall_info
 
 
-def public_photo_to_group_vk():
+def public_photo_to_group_vk(attachments, token):
+
     pass
 
 
@@ -99,5 +100,12 @@ if __name__ == "__main__":
     url_for_upload = test_info_response.get("upload_url")
 
     photo_info = upload_photo_to_vk(url_for_upload,  comics_information['name'])
-
     load_photo_to_server_vk = photo_save_wall_vk(group, photo_info, vk_access_token)
+    vk_server_response = load_photo_to_server_vk.get('response')
+    owner_id = vk_server_response[0].get("owner_id")
+    media_id = vk_server_response[0].get("id")
+
+    attachments = {
+        "media_id": media_id,
+        "owner_id": owner_id
+    }
